6e939cb33f0247d9f2096bc40f08b1bf0aa6f09c
ronniedroid <ronni.n.yousif@gmail.com>
2020-04-03 17:00:29 +0300

    added clipboard patch

--- x.c
+++ x.c
@@ -673,6 +673,8 @@ setsel(char *str, Time t)
 	XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, t);
 	if (XGetSelectionOwner(xw.dpy, XA_PRIMARY) != xw.win)
 		selclear();
+
+  clipcopy(NULL);
 }
 
 void
@@ -691,6 +693,8 @@ brelease(XEvent *e)
 
 	if (mouseaction(e, 1))
 		return;
+	if (e->xbutton.button == Button2)
+    clippaste(NULL);
 	if (e->xbutton.button == Button1)
 		mousesel(e, 1);
 }
